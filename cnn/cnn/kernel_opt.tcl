2e264a6809d23513e185d33adbd3eb23fd3d53ff
Sean <seanyukigeek@gmail.com>
2016-02-29 16:34:23 +0800

    fix bug in tcl

--- cnn/cnn/kernel_opt.tcl
+++ cnn/cnn/kernel_opt.tcl
@@ -32,7 +32,7 @@ add_files -kernel [get_kernels conv1] "conv1.cl"
 # Define binary containers.
 create_opencl_binary alpha
 set_property region "OCL_REGION_0" [get_opencl_binary alpha]
-create_compute_unit -opencl_binary [get_opencl_binary alpha] -kernel [get_kernels convolution_kernel_opt_1] -name ZW
+create_compute_unit -opencl_binary [get_opencl_binary alpha] -kernel [get_kernels conv1] -name ZW
 
 # Compile the design for CPU based emulation.
 compile_emulation -flow cpu -opencl_binary [get_opencl_binary alpha]
@@ -41,10 +41,10 @@ compile_emulation -flow cpu -opencl_binary [get_opencl_binary alpha]
 report_estimate
 
 # Run the design in CPU emulation mode
-run_emulation -flow cpu -args "../../../../../convolution.xml result.xml alpha.xclbin"
+run_emulation -flow cpu -args "../../../../../conv1.xml result.xml alpha.xclbin"
 
 build_system
 
 package_system
 
-run_system -args "../../../../convolution.xml result.xml alpha.xclbin"
+run_system -args "../../../../conv1.xml result.xml alpha.xclbin"
